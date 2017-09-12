819c4f98390e7dbf4a97f51319c43672511ee6c2
Peter Stovka <stovka.peter@gmail.com>
2017-09-12 11:28:45 +0200

    init

---
+++ src/main/kotlin/milan/App.kt
@@ -0,0 +1,36 @@
+package milan
+
+fun main(args: Array<String>) {
+    val app = App()
+
+    app.run()
+}
+
+class App {
+
+    private val container = Container()
+
+    fun run() {
+
+        println("Hello, my name is Milan, and I will tell you what's in today menu. Just give me a sec…")
+
+        val menuRepo = container.resolveMenuProvider()
+        val menu = menuRepo.getMenu()
+        val menuRenderer = container.resolveMenuRenderer()
+
+        menuRenderer.renderMenu(menu)
+    }
+
+}
+
+class Container {
+
+    fun resolveMenuProvider(): MenuProvider {
+        return KolegiumMenuProvider()
+    }
+
+    fun resolveMenuRenderer(): MenuRenderer {
+        return ConsoleMenuRenderer()
+    }
+
+}
